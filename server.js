f48c9f9a94c82dbcd0dd742c01d83d2099f848f9
Austin Robinson <austin.robinson@egov.com>
2022-04-25 15:38:40 -0500

    first commit from pro

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = express();
 const connectDB = require('./config/db');
 
 connectDB();
-app.get('/')
+// app.get('/')
 app.use(express.json({ extended: false}));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/licenses', require('./routes/licenses'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
