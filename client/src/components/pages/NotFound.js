7435bb31dbc6406468430707d704362000b3f38f
Austin Robinson <austin.robinson@egov.com>
2022-04-26 22:55:02 -0500

    fixed directories

---
+++ client/src/components/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <>
+        <h1 style={{marginTop: '50px'}} className='text-center'>Error: 404 page not found.</h1>
+    </>
+  )
+}
+
+export default NotFound
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const connectDB = require('./config/db');
 
 connectDB();
-app.use(express.json({ extended: false}));
+app.use(express.json({ extended: false }));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/licenses', require('./routes/licenses'));
